27944bcec6da86159ff24134cd649c3e7016e5d0
Shahrad Elahi <shahradq@gmail.com>
2024-05-23 15:41:45 +0330

    feat: make it work like a promise constructor

--- index.test.ts
+++ index.test.ts
@@ -13,6 +13,35 @@ async function fn(): Promise<SafeReturn<number, Error>> {
   return { error: new Error('An error') };
 }
 
+class MathError extends Error {
+  name = 'NerdsOnlyError';
+}
+
+function safePrime(num: number) {
+  /** This function is meant to act like a Promise constructor */
+  return trySafe<number, MathError>(async (resolve, reject) => {
+    const err = new MathError(`${num} is not prime`);
+
+    if (num <= 1 || num % 2 === 0 || num % 3 === 0) {
+      // return reject('Not prime'); // TS2345: Argument of type `string` is not assignable to parameter of type `MathError`
+      return reject(err);
+    }
+
+    if (num <= 3) {
+      return resolve(num);
+    }
+
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) {
+        return reject(err);
+      }
+    }
+
+    // return resolve('Prime'); // TS2345: Argument of type `string` is not assignable to parameter of type `number`
+    return resolve(num);
+  });
+}
+
 it('Must match typeof return value with SafeReturn', async () => {
   const res = await trySafe(fn);
 
@@ -57,9 +86,12 @@ it('should Must return type with SafeReturn', async () => {
 });
 
 it('should have a name', async () => {
-  const re = await trySafe(async () => {
+  const re = await trySafe(async (_, reject) => {
     await promiseThatMightReject();
-    await fetch('...');
+    const { statusText } = await fetch('...');
+    if (statusText !== 'OK') {
+      reject(new Error('Request failed for a silly reason'));
+    }
   });
 
   expectType<SafeReturn<void, Error>>(re);
@@ -78,6 +110,21 @@ it('should have a name', async () => {
 
   expect(re.data).to.be.undefined;
   expect(re.error).to.be.undefined;
+});
+
+it('should work like a promise constructor', async () => {
+  const { data, error } = await safePrime(7);
+
+  if (typeof data === 'undefined') {
+    expectType<MathError>(error);
+  }
+
+  if (typeof error === 'undefined') {
+    expectType<number>(data);
+  }
+
+  expect(error).to.be.undefined;
+  expect(data).to.be.a('number');
 });
 
 async function promiseThatMightReject() {
